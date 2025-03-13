0b8678a169687579348f3afbe9444bb0f41af39a
Mickey Mouse <mickeymouse@mac.resource.campus.njit.edu>
2025-03-13 12:29:56 -0400

    Added multiply and divide functions

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -4,3 +4,12 @@ def add(a,b):
 
 def subtract(a,b):
     return a - b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    try:
+        return a/b
+    except ZeroDivisionError:
+        return "Error: Cannot Divide by Zero"
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,10 +1,23 @@
 '''Test File'''
-from calculator import add,subtract
+from calculator import add,subtract,multiply,divide
 
 def test_addition():
     '''Returns he sum of two numbers'''
     assert add(2,6) == 8
 
 def test_subtraction():
-    '''Test that addition function works '''    
+    '''Test that subtraction function works '''    
     assert subtract(2,2) == 0
+
+def test_multiplication():
+    '''Test that multiplication function works '''    
+    assert multiply(2,2) == 4
+
+def test_division():
+    '''Test that division function works '''    
+    assert divide(2,2) == 1
+
+def test_division_by_zero():
+    '''Test that division function works '''    
+    assert divide(2,0) == "Error: Cannot Divide by Zero"
+    
