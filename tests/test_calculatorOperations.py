ca3d8055af051378462f02b54546bcf7d87107f8
Mickey Mouse <mickeymouse@mac.resource.campus.njit.edu>
2025-03-13 13:08:58 -0400

    calculator functionality using constructor and operations file

--- tests/test_calculatorOperations.py
+++ tests/test_calculatorOperations.py
@@ -1,22 +1,22 @@
 '''My Calculator Test'''
-from calculator import Calculator
+from calculator.calculatorOperation import add,subtract,multiply,divide
 
 def test_add():
     '''Test that add function works '''    
-    assert Calculator.add(2,2) == 4
+    assert add(2,2) == 4
 
 def test_subtract():
     '''Test that subtract function works '''    
-    assert Calculator.subtract(2,2) == 0
+    assert subtract(2,2) == 0
 
 def test_multiply():
     '''Test that multiply function works '''    
-    assert Calculator.multiply(2,2) == 4
+    assert multiply(2,2) == 4
 
 def test_divide():
     '''Test that divide function works '''    
-    assert Calculator.divide(2,2) == 1
+    assert divide(2,2) == 1
 
 def test_divide_by_zero():
-    '''Test that divide by zero function works '''
-    assert Calculator.divide(2,0) == "Error: Cannot Divide by Zero"
+    '''Test that divide by zero function works '''    
+    assert divide(2,0) == "Error: Cannot Divide by Zero"
